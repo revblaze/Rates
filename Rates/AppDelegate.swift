9443c314305002b3deb8c243e5fbc94bf83b4c1a
Justin Bush <revblaze@users.noreply.github.com>
2023-07-07 17:41:38 -0600

    feat: other minor changes

--- Rates/AppDelegate.swift
+++ Rates/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
   
   func applicationDidFinishLaunching(_ aNotification: Notification) {
     // Insert code here to initialize your application
+    let downloadManager = DownloadManager()
+    downloadManager.removeAllFilesInDocumentDirectory()
   }
   
   func applicationWillTerminate(_ aNotification: Notification) {
