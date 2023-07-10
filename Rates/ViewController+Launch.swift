a961c3af9d416b9c73d6c0d047eb961632bb64b6
Justin Bush <revblaze@users.noreply.github.com>
2023-07-10 14:20:40 -0600

    move: ViewController+Launch

--- Rates/ViewController+Launch.swift
+++ Rates/ViewController+Launch.swift
@@ -9,6 +9,97 @@ import Foundation
 
 extension ViewController {
   
+  // MARK: Perform at Launch
   
+  func beginLaunchSession() {
+    fillLaunchTableViewWithExchangeRateData()
+    
+    updateStatusBar(withState: .loading)
+    // If a day has passed since last updating the exchange rate data
+    if dataNeedsUpdating() {
+      checkInternetAndUpdateData()
+    }
+    // Else, data does not need updating
+    else {
+      checkLocalDataAndUpdateIfNecessary()
+    }
+  }
+  
+  func checkInternetAndUpdateData() {
+    // If there is no internet connection
+    if noInternetConnection() {
+      // But there does exist a local database
+      if localVersionOfDatabaseExists() {
+        // Update status bar to show caution message
+        updateStatusBar(withState: .noConnectionAndPrefersUpdate)
+      } else {
+        // Else, no internet or db > error message
+        updateStatusBar(withState: .noConnectionAndNoDb)
+      }
+    }
+    // Else, there is internet
+    else {
+      // Start download and conversion
+      updateStatusBar(withState: .isCurrentlyUpdating)
+      startCsvDownloadAndConvertToDb()
+    }
+  }
+  
+  func checkLocalDataAndUpdateIfNecessary() {
+    // If local database exists
+    if localVersionOfDatabaseExists() {
+      // Update status bar with "Up to date"
+      updateStatusBar(withState: .upToDate)
+    }
+    // Else, if data does not exist
+    else {
+      checkInternetAndUpdateData()
+    }
+  }
+  
+  func localVersionOfDatabaseExists() -> Bool {
+    let fileManager = FileManager.default
+    let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    do {
+      let directoryContents = try fileManager.contentsOfDirectory(at: documentsDirectoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+      
+      if directoryContents.isEmpty || !directoryContents.contains(where: { $0.lastPathComponent == "data.db" }) {
+        Debug.log("Documents directory is empty or does not contain 'data.db'")
+      } else {
+        Debug.log("Documents directory is not empty and contains 'data.db'")
+        return true
+      }
+    } catch {
+      Debug.log("Error accessing documents directory: \(error)")
+    }
+    return false
+  }
+  
+  func startCsvDownloadAndConvertToDb() {
+    Task.detached {
+      let exchangeRateData = ExchangeRateData()
+      if let dbFileUrl = await exchangeRateData.getDb(fromUrl: Settings.defaultExchangeRatesUrlString) {
+        Debug.log("Db file obtained: \(dbFileUrl)")
+        //await self.updateCSVTableViewWithCSV(at: dbFileUrl)
+        await self.fillLaunchTableViewWithExchangeRateData()
+        
+      } else {
+        Debug.log("Error occurred while awaiting getDb()")
+        await self.updateStatusBar(withState: .failedToUpdate)
+        
+      }
+    }
+  }
+  
+  func dataNeedsUpdating() -> Bool {
+    if DailyCheck.shouldPerformAction() {
+      Debug.log("[DailyCheck] New Day: download new exchange rate data")
+      return true
+    } else {
+      Debug.log("[DailyCheck] Same Day: no action required")
+      return false
+    }
+  }
   
 }
--- Rates/ViewController.swift
+++ Rates/ViewController.swift
@@ -28,6 +28,25 @@ class ViewController: NSViewController {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
+    initCsvTableScrollView()
+  }
+  
+  override func viewDidAppear() {
+    beginLaunchSession()
+  }
+  
+  
+  
+  
+  
+  override var representedObject: Any? {
+    didSet {
+      // Update the view, if already loaded.
+    }
+  }
+  
+  
+  func initCsvTableScrollView() {
     scrollView = NSScrollView(frame: view.bounds)
     scrollView.autoresizingMask = [.width, .height]
     scrollView.hasVerticalScroller = true
@@ -43,125 +62,5 @@ class ViewController: NSViewController {
     scrollView.documentView = csvTableView
     
     view.addSubview(scrollView, positioned: .below, relativeTo: nil)
-  }
-  
-  override func viewDidAppear() {
-    beginLaunchSession()
-  }
-  
-  // MARK: Perform at Launch
-  func beginLaunchSession() {
-    fillLaunchTableViewWithExchangeRateData()
-    
-    updateStatusBar(withState: .loading)
-    // If a day has passed since last updating the exchange rate data
-    if dataNeedsUpdating() {
-      checkInternetAndUpdateData()
-    }
-    // Else, data does not need updating
-    else {
-      checkLocalDataAndUpdateIfNecessary()
-    }
-  }
-  
-  func checkInternetAndUpdateData() {
-    // If there is no internet connection
-    if noInternetConnection() {
-      // But there does exist a local database
-      if localVersionOfDatabaseExists() {
-        // Update status bar to show caution message
-        updateStatusBar(withState: .noConnectionAndPrefersUpdate)
-      } else {
-        // Else, no internet or db > error message
-        updateStatusBar(withState: .noConnectionAndNoDb)
-      }
-    }
-    // Else, there is internet
-    else {
-      // Start download and conversion
-      updateStatusBar(withState: .isCurrentlyUpdating)
-      startCsvDownloadAndConvertToDb()
-    }
-  }
-  
-  func checkLocalDataAndUpdateIfNecessary() {
-    // If local database exists
-    if localVersionOfDatabaseExists() {
-      // Update status bar with "Up to date"
-      updateStatusBar(withState: .upToDate)
-    }
-    // Else, if data does not exist
-    else {
-      checkInternetAndUpdateData()
-    }
-  }
-  
-  func localVersionOfDatabaseExists() -> Bool {
-    let fileManager = FileManager.default
-    let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-    
-    do {
-      let directoryContents = try fileManager.contentsOfDirectory(at: documentsDirectoryURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-      
-      if directoryContents.isEmpty || !directoryContents.contains(where: { $0.lastPathComponent == "data.db" }) {
-        Debug.log("Documents directory is empty or does not contain 'data.db'")
-      } else {
-        Debug.log("Documents directory is not empty and contains 'data.db'")
-        return true
-      }
-    } catch {
-      Debug.log("Error accessing documents directory: \(error)")
-    }
-    return false
-  }
-  
-  func startCsvDownloadAndConvertToDb() {
-    Task.detached {
-      let exchangeRateData = ExchangeRateData()
-      if let dbFileUrl = await exchangeRateData.getDb(fromUrl: Settings.defaultExchangeRatesUrlString) {
-        Debug.log("Db file obtained: \(dbFileUrl)")
-        //await self.updateCSVTableViewWithCSV(at: dbFileUrl)
-        await self.fillLaunchTableViewWithExchangeRateData()
-        
-      } else {
-        Debug.log("Error occurred while awaiting getDb()")
-        await self.updateStatusBar(withState: .failedToUpdate)
-        
-      }
-    }
-  }
-  
-  func dataNeedsUpdating() -> Bool {
-    if DailyCheck.shouldPerformAction() {
-      Debug.log("[DailyCheck] New Day: download new exchange rate data")
-      return true
-    } else {
-      Debug.log("[DailyCheck] Same Day: no action required")
-      return false
-    }
-  }
-  
-  func findDataDBFileURL() -> URL? {
-    do {
-      let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-      let fileURLs = try FileManager.default.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-      
-      // Filter fileURLs to find the file named "data.db"
-      if let dataDBURL = fileURLs.first(where: { $0.lastPathComponent == "data.db" }) {
-        return dataDBURL
-      }
-    } catch {
-      print("Error while searching for data.db file: \(error)")
-    }
-    
-    return nil
-  }
-  
-  
-  
-  override var representedObject: Any? {
-    didSet {
-      // Update the view, if already loaded.
-    }
   }
 }
