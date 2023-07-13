3ce3145b3953abc71696173b25b20fa47175f30e
Justin Bush <revblaze@users.noreply.github.com>
2023-07-13 16:29:19 -0600

    refactor: cleaner Constants

--- Rates/Settings/Constants.swift
+++ Rates/Settings/Constants.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 struct Constants {
-
-  static let CSV_URL_STRING = "https://www.bis.org/statistics/full_xru_d_csv_row.zip"
   
-  static let DEFAULT_DATE = DateComponents(year: 2016, month: 1, day: 1).date!
+  static let csvUrlString = "https://www.bis.org/statistics/full_xru_d_csv_row.zip"
+  static let defaultDate = DateComponents(year: 2016, month: 1, day: 1).date!
+  
+  // MARK: Utility Constants
+  static let alphaNumericString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  static let mainStoryboard = "Main"
+  static let importFileTemplateViewControllerIdentifier = "ImportFileTemplateViewController"
   
 }
