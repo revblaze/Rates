ccd45b56c9f692fc3553c35698bd2ded2c7a77cf
Justin Bush <revblaze@users.noreply.github.com>
2023-07-13 16:04:47 -0600

    refactor: clean WindowController

--- Rates/WindowController.swift
+++ Rates/WindowController.swift
@@ -7,35 +7,40 @@
 
 import Cocoa
 
+// MARK: - WindowController
 class WindowController: NSWindowController, FileSelectionDelegate {
   
   lazy var viewController: ViewController = {
-    return self.window?.contentViewController as! ViewController
+    let vc = self.window?.contentViewController as! ViewController
+    vc.delegate = self
+    return vc
   }()
   
   @IBAction func openFileAction(_ sender: Any) {
-    callOpenFileSelection()
+    performActionOnViewController(action: viewController.openFileSelection)
   }
   
   @IBAction func revertTableViewChanges(_ sender: Any) {
-    viewController.delegate = self
-    viewController.revertTableViewChanges()
+    performActionOnViewController(action: viewController.revertTableViewChanges)
   }
   
   @IBAction func filterAppStoreConnectSales(_ sender: Any) {
-    viewController.delegate = self
-    viewController.filterAppStoreConnectSales()
+    performActionOnViewController(action: viewController.filterAppStoreConnectSales)
   }
   
   // Function to call the file selection in ViewController
   func callOpenFileSelection() {
-    viewController.delegate = self
-    viewController.openFileSelection()
+    performActionOnViewController(action: viewController.openFileSelection)
   }
   
   // Implement the FileSelectionDelegate method
   func fileSelected(_ viewController: ViewController, fileURL: URL) {
     Debug.log("Selected file: \(fileURL)")
     // Process the selected file URL
+  }
+  
+  // Helper method to avoid code duplication
+  private func performActionOnViewController(action: () -> ()) {
+    action()
   }
 }
