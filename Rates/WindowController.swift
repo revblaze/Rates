767cbdd608e792a7ffd74c5c5e0b7d17fc16766e
Justin Bush <revblaze@users.noreply.github.com>
2023-07-14 11:31:45 -0600

    feat: WindowController buttons, actions

--- Rates/WindowController.swift
+++ Rates/WindowController.swift
@@ -10,6 +10,8 @@ import Cocoa
 /// The main window controller for the application.
 class WindowController: NSWindowController, FileSelectionDelegate {
   
+  @IBOutlet weak var selectCustomHeaderRowButton: NSToolbarItem!
+  
   /// The main view controller of the window.
   lazy var viewController: ViewController = {
     let vc = self.window?.contentViewController as! ViewController
@@ -32,8 +34,9 @@ class WindowController: NSWindowController, FileSelectionDelegate {
     performActionOnViewController(action: viewController.filterAppStoreConnectSales)
   }
   
+  /// Performs an action on the view controller to manually select the current table view row as the header row.
   @IBAction func selectCustomHeaderRowFromTable(_ sender: Any) {
-    
+    performActionOnViewController(action: viewController.selectCustomHeaderForTableView)
   }
   
   /// Calls the file selection in the view controller.
