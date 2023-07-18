c1af3b079567ab8e9830b53edf93bc2e8b20e7d9
Justin Bush <revblaze@users.noreply.github.com>
2023-07-17 21:20:17 -0600

    feat: deincrement day by 1 (max: 5)

--- Rates/Query/QueryUsdExchangeRate.swift
+++ Rates/Query/QueryUsdExchangeRate.swift
@@ -47,33 +47,46 @@ extension Query {
       dateFormatter.dateFormat = format
       
       // If the date string can be parsed, convert it to the desired format
-      if let date = dateFormatter.date(from: date) {
+      if var date = dateFormatter.date(from: date) {
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        let formattedDate = dateFormatter.string(from: date)
         
-        do {
-          // Connect to the SQLite database if the URL is available
-          if let url = Query.sqliteUrl()?.path {
-            let db = try Connection(url)
-            
-            // Construct the SQL query
-            let query = "SELECT \(code) FROM data WHERE Currency = ?"
-            
-            // Prepare the statement
-            let stmt = try db.prepare(query)
-            
-            // Bind the date to the statement
-            _ = stmt.bind(formattedDate)
-            
-            // Run the query and fetch the result
-            if let row = stmt.next() {
-              return Double(row[0] as? String ?? "0.0") ?? 0.0
+        var attempts = 0
+        while attempts < 5 {
+          let formattedDate = dateFormatter.string(from: date)
+          
+          do {
+            // Connect to the SQLite database if the URL is available
+            if let url = Query.sqliteUrl()?.path {
+              let db = try Connection(url)
+              
+              // Construct the SQL query
+              let query = "SELECT \(code) FROM data WHERE Currency = ?"
+              
+              // Prepare the statement
+              let stmt = try db.prepare(query)
+              
+              // Bind the date to the statement
+              _ = stmt.bind(formattedDate)
+              
+              // Run the query and fetch the result
+              if let row = stmt.next() {
+                let result = row[0] as? String
+                
+                // If the result is not "NaN" or empty, return it as a double
+                if result != "NaN" && result != "" {
+                  return Double(result ?? "0.0") ?? 0.0
+                }
+              }
+            } else {
+              Debug.log("[Query.usdExchangeRate] SQLite URL is nil")
             }
-          } else {
-            Debug.log("[Query.usdExchangeRate] SQLite URL is nil")
+          } catch {
+            Debug.log("[Query.usdExchangeRate] Database connection or query execution failed: \(error)")
           }
-        } catch {
-          Debug.log("[Query.usdExchangeRate] Database connection or query execution failed: \(error)")
+          
+          // If the result was "NaN" or empty, decrement the date and try again
+          date = Calendar.current.date(byAdding: .day, value: -1, to: date)!
+          attempts += 1
         }
       }
     }
