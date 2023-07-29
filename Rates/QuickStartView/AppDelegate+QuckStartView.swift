5f58a37089abefc89c73626f2bc3e19487c7fcaf
Justin Bush <revblaze@users.noreply.github.com>
2023-07-29 10:17:36 -0600

    feat: QuickStartView, calls and implementation

---
+++ Rates/QuickStartView/AppDelegate+QuckStartView.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate+QuckStartView.swift
+//  Rates
+//
+//  Created by Justin Bush on 7/29/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension AppDelegate {
+  
+  /// Presents the QuickStartView as a new window.
+  func presentQuickStartViewAsWindow() {
+    // Create an instance of your SwiftUI view
+    let contentView = QuickStartView()
+    
+    // Create a new window
+    let window = NSWindow(
+      contentRect: NSRect(x: 0, y: 0, width: 400, height: 600),
+      styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+      backing: .buffered, defer: false)
+    window.center()
+    window.setFrameAutosaveName(Constants.quickStartViewHostingControllerIdentifier)
+    window.contentView = NSHostingView(rootView: contentView)
+    window.makeKeyAndOrderFront(nil)
+    
+    // Set minimum window size
+    window.minSize = NSSize(width: 300, height: 400)
+    
+    // Assign the window to a property on AppDelegate to prevent it from being deallocated
+    self.quickStartWindow = window
+  }
+  
+}
---
+++ Rates/QuickStartView/QuickStartView.swift
@@ -0,0 +1,126 @@
+//
+//  QuickStartView.swift
+//  Rates
+//
+//  Created by Justin Bush on 7/29/23.
+//
+
+import SwiftUI
+
+extension Constants {
+  
+  static let quickStartViewGenericPadding = CGFloat(8)
+  
+}
+
+struct QuickStartView: View {
+  var body: some View {
+    VStack {
+      ScrollView {
+        
+        ExpandableSectionView(title: "Section 1 Title", contentView: StepImageCaptionRow(header: "Section 1, Header 1", imageString: "sample", caption: "This is caption text."), expanded: true)
+        ExpandableSectionView(title: "Section 2 Title", contentView: StepImageCaptionRow(header: "Section 2, Header 1", imageString: "sample", caption: "This is caption text."))
+        ExpandableSectionView(title: "Section 3 Title", contentView: StepImageCaptionRow(header: "Section 3, Header 1", imageString: "sample", caption: "This is caption text."))
+      }
+    }
+    .frame(minWidth: 300, minHeight: 400)
+  }
+}
+
+struct StepImageCaptionRow: View {
+  var header: String
+  var imageString: String?
+  var caption: String?
+  
+  @State private var showImagePopover: Bool = false
+  
+  var body: some View {
+    VStack {
+      
+      HStack {
+        Text(header)
+          .bold()
+          .fixedSize(horizontal: false, vertical: true)
+        Spacer()
+      }
+      
+      if let imageString = imageString {
+        Image(imageString)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .padding(.vertical, Constants.quickStartViewGenericPadding)
+          .onTapGesture {
+            showImagePopover = true
+          }
+          .popover(isPresented: $showImagePopover) {
+            Image(imageString)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+          }
+      }
+      
+      if let caption = caption {
+        HStack {
+          Text(caption)
+            .font(.caption)
+            .fixedSize(horizontal: false, vertical: true)
+          Spacer()
+        }
+        .padding(.top, Constants.quickStartViewGenericPadding)
+      }
+      
+    }
+    .padding(Constants.quickStartViewGenericPadding)
+    
+  }
+}
+
+
+
+struct ExpandableSectionView: View {
+  var title: String
+  var contentView: StepImageCaptionRow
+  var expanded: Bool?
+  
+  @State private var isExpanded: Bool
+  
+  init(title: String, contentView: StepImageCaptionRow, expanded: Bool? = nil) {
+    self.title = title
+    self.contentView = contentView
+    self.expanded = expanded
+    self._isExpanded = State(initialValue: expanded ?? false)
+  }
+  
+  var body: some View {
+    VStack {
+      Button(action: {
+        withAnimation {
+          isExpanded.toggle()
+        }
+      }) {
+        HStack {
+          Text(title)
+            .font(.headline)
+          Spacer()
+          Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
+        }
+      }
+      .buttonStyle(PlainButtonStyle())
+      .frame(maxWidth: .infinity)
+      
+      if isExpanded {
+        VStack {
+          contentView
+        }
+        .transition(.opacity)
+      }
+    }
+    .padding()
+  }
+}
+
+//struct QuickStartView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    QuickStartView()
+//  }
+//}
